25fe1cffa979e427ccf48046c7a21c2de1930cfa
delude88 <tobias.hegemann@googlemail.com>
2022-09-29 17:19:09 +0200

    Replaced cmake with emcmake

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
+# Build the wasm module
 
+# Setup lib
 pushd lib || exit
   . setup.sh
 popd || exit
 
-fetch_brew_dependency() {
-    FORMULA_NAME=$1
+# macOS: Install dependencies via brew
+if [[ $OSTYPE == 'darwin'* ]]; then
+  fetch_brew_dependency() {
+      FORMULA_NAME=$1
 
-    echo "Fetching Brew dependency: '$FORMULA_NAME'."
+      echo "Fetching Brew dependency: '$FORMULA_NAME'."
 
-    if brew ls --versions $FORMULA_NAME > /dev/null; then
-        echo "Dependency '$FORMULA_NAME' is already installed, continuing ..."
-    else
-        echo "Dependency '$FORMULA_NAME' is not installed, installing via Homebrew ..."
-        brew install $FORMULA_NAME
-    fi
-}
+      if brew ls --versions $FORMULA_NAME > /dev/null; then
+          echo "Dependency '$FORMULA_NAME' is already installed, continuing ..."
+      else
+          echo "Dependency '$FORMULA_NAME' is not installed, installing via Homebrew ..."
+          brew install $FORMULA_NAME
+      fi
+  }
 
-# Install required toolset
-fetch_brew_dependency "cmake"
-fetch_brew_dependency "emscripten"
-pushd lib || exit
-  bash setup.sh
-popd || exit
-
-# Make sure we have a 'build' folder.
-if [ ! -d "build" ]; then
-    mkdir build
+  # Install required toolset
+  fetch_brew_dependency "cmake"
+  fetch_brew_dependency "emscripten"
 fi
 
-# Build using emscripten shipped by brew and cmake
-EMSCRIPTEN=$(brew --prefix emscripten)
-EMSCRIPTEN_CMAKE_PATH=${EMSCRIPTEN}/libexec/cmake/Modules/Platform/Emscripten.cmake
+# Make sure we have a 'build' folder.
+mkdir -p build
+
+# Now build
 pushd build || exit
-  echo "Emscripten CMake path: ${EMSCRIPTEN_CMAKE_PATH}"
-  cmake -DCMAKE_TOOLCHAIN_FILE=${EMSCRIPTEN_CMAKE_PATH} ..
+  emcmake cmake ..
   echo "Building project ..."
-  make
+  emmake make
 popd || exit
